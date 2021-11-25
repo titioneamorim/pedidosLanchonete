04d8aca4637cba5a9e96cbbcbb28231c5822d6cf
titioneamorim <titione@gmail.com.br>
2021-11-25 13:23:58 -0300

    add

--- customer/services.py
+++ customer/services.py
@@ -5,18 +5,15 @@ from .models import CustomerModel
 
 
 class CustomerServices():
-    
-    def salvar_customer(self, customer:CustomerSerializer):
+
+    def salvar_customer(self, customer: CustomerSerializer):
         customer.save()
-    
+
     def search_all_customers(self) -> list[CustomerModel]:
         return CustomerModel.objects.all()
-    
+
     def search_customer_by_id(self, id) -> CustomerModel:
         return CustomerModel.objects.filter(id=id).first()
-    
+
     def delete_customer(self, customer_id):
         return CustomerModel.objects.filter(id=customer_id).delete()
-    
-    
-        
