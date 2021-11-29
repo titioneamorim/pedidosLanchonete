b185b449cc89f5fa9100dc4e0fac8534fe439733
Marcelo <pac57282@gmail.com>
2021-11-29 11:59:38 -0300

    finalizado produtos

--- product/admin.py
+++ product/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import ProductModel
 
-admin.site.register(ProductModel)
+@admin.register(ProductModel)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    list_filter = ['name', 'price']
