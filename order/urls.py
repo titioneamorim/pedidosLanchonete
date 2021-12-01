47508d68727b43176818984ff6cc43d2ba7a73ff
Marcelo <pac57282@gmail.com>
2021-11-30 21:35:51 -0300

    mmm

--- order/urls.py
+++ order/urls.py
@@ -5,11 +5,7 @@ namespace = 'order'
 
 urlpatterns = [
     path('orders/', home_order, name="home_orders"),
-<<<<<<< HEAD
-    path('products/edit/<uuid:id>/', update_order_screen, name="edit_orders"),
-=======
     path('products/edit/<uuid:id>/', update_order_screen, name="edit_order"),
->>>>>>> 6e20def13dce25cd17aebec6fea9726d7e7a39de
     path('products/save', save_order, name="save_order"),
     path('products/search/', search_order_by_therm, name="search_orders"),
 ]
