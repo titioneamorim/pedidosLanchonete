4f1033572e11d277d16af3e5c5d62e61f3207c6f
titioneamorim <titione@gmail.com.br>
2021-11-30 21:32:20 -0300

    c

--- order/urls.py
+++ order/urls.py
@@ -6,6 +6,6 @@ namespace = 'order'
 urlpatterns = [
     path('orders/', home_order, name="home_orders"),
     path('products/edit/<uuid:id>/', update_order_screen, name="edit_orders"),
-    path('products/save', save_order, name="save_orders"),
+    path('products/save', save_order, name="save_order"),
     path('products/search/', search_order_by_therm, name="search_orders"),
 ]
